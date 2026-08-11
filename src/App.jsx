f2804b761446acbe6c15937e9a6243506c5ead0f
armarketingsolutions8 <armarketingsolutions8@gmail.com>
2026-08-12 04:05:33 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -431,7 +431,7 @@ const App = () => {
             <RevealOnScroll delay={300} className="group bg-slate-50 rounded-2xl border border-slate-100 shadow-md hover:shadow-xl transition-all overflow-hidden flex flex-col hover:-translate-y-2 duration-300">
               <div className="h-48 overflow-hidden relative">
                 <img 
-                  src= "/ce3.jpeg" 
+                  src= "/ce3.png" 
                   alt="Sludge Oil Recovery" 
                   className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-700"
                 />
